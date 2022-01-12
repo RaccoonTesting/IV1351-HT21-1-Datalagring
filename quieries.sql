37c00d1ab2c2034b4af20bea2129c1da4c645476
Johanna Schubert <jschub@kth.se>
2022-01-12 09:14:00 +0100

    Update quieries.sql

--- quieries.sql
+++ quieries.sql
@@ -25,6 +25,8 @@ ORDER BY m) sub
 WHERE m >= '2019-01-01'
 AND m < '2020-01-01';
 
+
+
 SELECT * FROM (
 SELECT COUNT(*) as numlessons, instructor_id FROM events
 WHERE start_time >= date_trunc('month', CURRENT_DATE)
@@ -33,6 +35,10 @@ ORDER BY numlessons ASC
 )as sub
 WHERE sub.numlessons <= 2
 
+
+
+
+
 SELECT lessons.subject, events.max_participants, events.min_participants, extract(isodow from events.start_time) as dayofweek FROM events
 INNER JOIN lessons on lessons.id= events.lesson_id
 WHERE start_time >= '2019-01-01'
@@ -40,16 +46,10 @@ AND start_time < '2019-02-01'
 
 GROUP BY extract(isodow from events.start_time), lessons.subject, events.max_participants, events.min_participants
 
-
 SELECT
 event_id,
 CASE
       WHEN  max_participants <= booked THEN 'full'
       WHEN  (max_participants - booked)<= 2 THEN '1 or 2 spots left '
       WHEN  (max_participants - booked)> 2 THEN 'more than 2 spots left '
-END FROM(
-SELECT
-count(*) as booked, event_attendance.event_id, events.max_participants
-FROM event_attendance
-INNER JOIN events on event_attendance.event_id = events.id
-GROUP BY (event_attendance.event_id, events.max_participants)) as t1
+END FROM booked;
